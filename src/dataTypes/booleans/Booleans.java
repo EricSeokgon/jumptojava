1eb450dcbe6d2455654ef5471478cc2c583766e6
hadesyi <hadesleeq@gmail.com>
2016-03-05 00:55:25 +0900

    부을 (boolean)

---
+++ src/dataTypes/booleans/Booleans.java
@@ -0,0 +1,24 @@
+package dataTypes.booleans;
+
+/**
+ * Project: jumptojava
+ * FileName: Booleans
+ * Date: 2016-03-05
+ * Time: 오전 12:49
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isSuccess = true;
+        boolean isTest = false;
+
+        System.out.println("isSuccess :" + isSuccess);
+        System.out.println("isTest :" + isTest);
+
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println("isOdd :" + isOdd);
+    }
+}
