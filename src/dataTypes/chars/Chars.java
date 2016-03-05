e18ffa834e736c36647b74a9b4d7b9f8396e6696
hadesyi <hadesleeq@gmail.com>
2016-03-06 03:06:48 +0900

    문자(char)

---
+++ src/dataTypes/chars/Chars.java
@@ -0,0 +1,22 @@
+package dataTypes.chars;
+
+/**
+ * Project: jumptojava
+ * FileName: Chars
+ * Date: 2016-03-06
+ * Time: 오전 3:02
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chars {
+    public static void main(String[] args) {
+        char a1 = 'a'; //문자값
+        char a2 = 97; //아스키코드값
+        char a3 = '\u0061'; //유니코드값
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
