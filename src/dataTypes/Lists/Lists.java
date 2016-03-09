06a3c586249c7e65e52722772d0ec9eb590dcf39
hadesyi <github1441>
2016-03-10 00:10:20 +0900

    리스트 (List) add

---
+++ src/dataTypes/Lists/Lists.java
@@ -0,0 +1,15 @@
+package dataTypes.Lists;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hadeslee on 2016-03-09.
+ */
+public class Lists {
+    public static void main(String[] args) {
+        ArrayList pitches = new ArrayList();
+        pitches.add("138");
+        pitches.add("129");
+        pitches.add("142");
+    }
+}
