d89697c49e14fc27bcb06348bda144c7ec8b7ea6
hadesyi <hadesleeq@gmail.com>
2016-03-14 00:12:48 +0900

    if 문

---
+++ src/Control/If.java
@@ -0,0 +1,21 @@
+package Control;
+
+/**
+ * Project: jumptojava
+ * FileName: If
+ * Date: 2016-03-14
+ * Time: 오전 12:11
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class If {
+    public static void main(String[] args) {
+        boolean money = true;
+        if (money) {
+            System.out.println("택시를 타고 가라");
+        } else {
+            System.out.println("걸어가라");
+        }
+    }
+}
