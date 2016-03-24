2662d813a85598aee75dd22082a9f34c87981619
hadesyi <hadesleeq@gmail.com>
2016-03-25 00:37:53 +0900

    인터페이스

---
+++ src/OOP/ZooKeeper.java
@@ -0,0 +1,20 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: ZooKeeper
+ * Date: 2016-03-25
+ * Time: 오전 12:35
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ZooKeeper {
+    public void feed(Tiger tiger) {
+        System.out.println("feed tiger");
+    }
+
+    public void feed(Lion lion) {
+        System.out.println("feed lion");
+    }
+}
