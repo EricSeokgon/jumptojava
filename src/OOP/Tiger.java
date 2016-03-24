b649e75f310056d806356525c83c07e99ca4fc1f
hadesyi <hadesleeq@gmail.com>
2016-03-25 00:34:59 +0900

    인터페이스

---
+++ src/OOP/Tiger.java
@@ -0,0 +1,13 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: Tiger
+ * Date: 2016-03-25
+ * Time: 오전 12:34
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tiger extends Animal {
+}
