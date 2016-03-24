1e968bd07602c3d4301f63e003090011f3d1400b
hadesyi <hadesleeq@gmail.com>
2016-03-25 00:42:12 +0900

    인터페이스

---
+++ src/OOP/Crocodile.java
@@ -0,0 +1,13 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: Crocodile
+ * Date: 2016-03-25
+ * Time: 오전 12:41
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Crocodile extends Animal implements Predator {
+}
