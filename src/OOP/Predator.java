7c3495951b29aa55eaf2b84aad3a246e0f3525ab
hadesyi <hadesleeq@gmail.com>
2016-03-25 00:39:03 +0900

    인터페이스

---
+++ src/OOP/Predator.java
@@ -0,0 +1,13 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: Predator
+ * Date: 2016-03-25
+ * Time: 오전 12:38
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Predator {
+}
