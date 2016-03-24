293b2da8343c5e4e45aef2f713c5c14d3fed4a9f
hadesyi <hadesleeq@gmail.com>
2016-03-25 00:35:21 +0900

    인터페이스

---
+++ src/OOP/Lion.java
@@ -0,0 +1,13 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: Lion
+ * Date: 2016-03-25
+ * Time: 오전 12:35
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lion extends Animal {
+}
