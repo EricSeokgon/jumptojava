8cdcd3f5516697cdf2d61b107d6685ae7f506b9c
hadesyi <hadesleeq@gmail.com>
2016-03-26 03:20:50 +0900

    다형성

--- src/OOP/Animal.java
+++ src/OOP/Animal.java
@@ -15,8 +15,4 @@ public class Animal {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
---
+++ src/OOP/BarkablePredator.java
@@ -0,0 +1,15 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: BarkablePredator
+ * Date: 2016-03-26
+ * Time: 오전 3:18
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface BarkablePredator {
+    public void bark();
+    public String getName();
+}
