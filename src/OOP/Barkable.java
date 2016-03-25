76bf8c13b95613450c873c4836c0bc80a0f6c4fa
hadesyi <hadesleeq@gmail.com>
2016-03-26 03:13:39 +0900

    다형성

---
+++ src/OOP/Barkable.java
@@ -0,0 +1,14 @@
+package OOP;
+
+/**
+ * Project: jumptojava
+ * FileName: Barkable
+ * Date: 2016-03-26
+ * Time: 오전 3:13
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Barkable {
+    public void bark();
+}
