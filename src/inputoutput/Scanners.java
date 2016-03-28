27f3c0db08622f0743ccd7fd62fe91f63558be5a
hadesyi <hadesleeq@gmail.com>
2016-03-29 00:20:58 +0900

    Scanner

---
+++ src/inputoutput/Scanners.java
@@ -0,0 +1,19 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+/**
+ * Project: jumptojava
+ * FileName: Scanners
+ * Date: 2016-03-29
+ * Time: 오전 12:19
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Scanners {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next());
+    }
+}
