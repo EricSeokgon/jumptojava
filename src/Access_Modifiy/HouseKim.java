038edcedeec1d9384dfba1b014554cc2c48abf8a
hadesyi <hadesleeq@gmail.com>
2016-04-05 03:19:53 +0900

    접근제어자 default

---
+++ src/Access_Modifiy/HouseKim.java
@@ -0,0 +1,14 @@
+package Access_Modifiy;
+
+/**
+ * Project: jumptojava
+ * FileName: HouseKim
+ * Date: 2016-04-05
+ * Time: 오전 3:17
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HouseKim {
+    String lastname = "kim";
+}
