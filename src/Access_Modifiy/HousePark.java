9ea7d8566d785a83557fa1aff5ec99a9dca12883
hadesyi <hadesleeq@gmail.com>
2016-04-05 03:21:23 +0900

    접근제어자 default

---
+++ src/Access_Modifiy/HousePark.java
@@ -0,0 +1,19 @@
+package Access_Modifiy;
+
+/**
+ * Project: jumptojava
+ * FileName: HousePark
+ * Date: 2016-04-05
+ * Time: 오전 3:20
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HousePark {
+    String lastname = "park";
+
+    public static void main(String[] args) {
+        final HouseKim kim = new HouseKim();
+        System.out.println(kim.lastname);
+    }
+}
