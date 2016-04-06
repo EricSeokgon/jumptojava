0d6c1aff99eb9c20bd35e84768f6f3b9d1219143
hadesyi <hadesleeq@gmail.com>
2016-04-07 00:08:25 +0900

    정적 변수와 메소드 (static)

--- src/statics/Counter.java
+++ src/statics/Counter.java
@@ -12,13 +12,17 @@ package statics;
 public class Counter {
     static int count = 0;
 
-    public Counter() {
+    Counter() {
         this.count++;
-        System.out.println(this.count);
+    }
+
+    public static int getCount() {
+        return count;
     }
 
     public static void main(String[] args) {
         final Counter c1 = new Counter();
         final Counter c2 = new Counter();
+        System.out.println(Counter.getCount());
     }
 }
