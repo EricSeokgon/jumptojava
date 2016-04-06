43fc1ac0f6111d8d50f9f611907875fc4a6fdbd5
hadesyi <hadesleeq@gmail.com>
2016-04-06 13:38:33 +0900

    정적 변수와 메소드 (static)

---
+++ src/statics/HousePark.java
@@ -0,0 +1,19 @@
+package statics;
+
+/**
+ * Project: jumptojava
+ * FileName: HousePark
+ * Date: 2016-04-06
+ * Time: 오후 1:37
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HousePark {
+    String lastname = "박";
+
+    public static void main(String[] args) {
+        HousePark pey = new HousePark();
+        HousePark pes = new HousePark();
+    }
+}
