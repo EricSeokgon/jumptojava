0278c46fde44b2ea19dd7aad13ab86f66e9c5535
hadesyi <hadesleeq@gmail.com>
2016-04-08 00:05:37 +0900

    싱글톤 패턴 (singleton pattern)

--- src/statics/SingletonTest.java
+++ src/statics/SingletonTest.java
@@ -10,18 +10,26 @@ package statics;
  * To change this template use File | Settings | File Templates.
  */
 class Singleton {
+    private static Singleton one;
+
     private Singleton() {
     }
 
     public static Singleton getInstance() {
-        return new Singleton();
+        if (one == null) {
+            one = new Singleton();
+        }
+        return one;
     }
 
 }
 
 public class SingletonTest {
     public static void main(String[] args) {
-        //final Singleton singleton = new Singleton();
-        final Singleton singleton = Singleton.getInstance();
+
+        final Singleton singleton1 = Singleton.getInstance();
+        final Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);
+        System.out.println(singleton1.equals(singleton2));
     }
 }
