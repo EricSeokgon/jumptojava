71a8102ed51a361375e352c22de7659a9f18a0c0
hadesyi <hadesleeq@gmail.com>
2016-04-09 00:11:03 +0900

    ArithmeticException

---
+++ src/exception/Test.java
@@ -0,0 +1,27 @@
+package exception;
+
+/**
+ * Project: jumptojava
+ * FileName: Test
+ * Date: 2016-04-09
+ * Time: 오전 12:09
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public void shouldBeRun(){
+        System.out.println("ok thanks.");
+    }
+
+    public static void main(String[] args) {
+        final Test test = new Test();
+        int c;
+        try {
+            c = 4 / 0;
+            test.shouldBeRun();
+        } catch (ArithmeticException e) {
+            c = -1;
+        }
+    }
+}
