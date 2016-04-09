4d7071f96d9a6295933dc7fccd2b445b38ef34fe
hadesyi <hadesleeq@gmail.com>
2016-04-10 00:15:22 +0900

    RuntimeException

---
+++ src/exception/FoolException.java
@@ -0,0 +1,13 @@
+package exception;
+
+/**
+ * Project: jumptojava
+ * FileName: FoolException
+ * Date: 2016-04-10
+ * Time: 오전 12:15
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class FoolException extends RuntimeException {
+}
