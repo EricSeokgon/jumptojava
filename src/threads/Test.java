195efc6b5b5527a8af788cb91f18d95c98da20c9
hadesyi <hadesleeq@gmail.com>
2016-04-16 06:35:39 +0900

    Runnable

--- src/threads/Test.java
+++ src/threads/Test.java
@@ -11,38 +11,33 @@ import java.util.ArrayList;
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-public class Test extends Thread {
+public class Test implements Runnable {
     int seq;
-
     public Test(int seq) {
         this.seq = seq;
     }
-
     public void run() {
-
-        System.out.println(this.seq + " thread start.");
+        System.out.println(this.seq+" thread start.");
         try {
             Thread.sleep(1000);
-        } catch (Exception e) {
-
+        }catch(Exception e) {
         }
-        System.out.println(this.seq + " thread end.");
-
+        System.out.println(this.seq+" thread end.");
     }
 
     public static void main(String[] args) {
         ArrayList<Thread> threads = new ArrayList<Thread>();
-        for (int i = 0; i < 10; i++) {
-            Thread t = new Test(i);
+        for(int i=0; i<10; i++) {
+            Thread t = new Thread(new Test(i));
             t.start();
             threads.add(t);
         }
 
-        for (int i = 0; i < threads.size(); i++) {
+        for(int i=0; i<threads.size(); i++) {
             Thread t = threads.get(i);
             try {
                 t.join();
-            } catch (Exception e) {
+            }catch(Exception e) {
             }
         }
         System.out.println("main end.");
