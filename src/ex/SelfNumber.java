e3d0c4e5ef9e895e5fa00de061d14aaa55051338
hadesyi <hadesleeq@gmail.com>
2016-04-19 02:45:06 +0900

    Self Number

---
+++ src/ex/SelfNumber.java
@@ -0,0 +1,41 @@
+package ex;
+
+import java.util.ArrayList;
+
+/**
+ * Project: jumptojava
+ * FileName: SelfNumber
+ * Date: 2016-04-19
+ * Time: 오전 2:42
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class SelfNumber {
+    private static ArrayList<Integer> numbersHasGenerator;
+
+    public static void main(String[] args) {
+        calculateNumbersHasGenerator();
+        int sum = 0;
+        for (int i = 0; i < 5001; i++)
+            if (!hasGenerator(i))
+                sum += i;
+        System.out.println("합 : " + sum);
+    }
+
+    private static boolean hasGenerator(int num) {
+        return numbersHasGenerator.contains(num);
+    }
+
+    private static void calculateNumbersHasGenerator() {
+        numbersHasGenerator = new ArrayList<Integer>();
+        for (int i = 0; i <= 5000; i++) {
+            String num = String.valueOf(i);
+            int no = 0;
+            for (int n = 0; n < num.length(); n++)
+                no += Integer.parseInt(num.substring(n, n + 1));
+            numbersHasGenerator.add(no + i);
+        }
+    }
+}
+
